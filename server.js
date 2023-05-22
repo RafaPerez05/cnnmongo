a9616db23b2ecd1d9bcf94e54b9c92494f034218
RafaPerez05 <126112322+RafaPerez05@users.noreply.github.com>
2023-05-22 16:41:52 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3003;
+
+const url = "mongodb+srv://rafaelsilva707:bkFBrM5UW8j1X6Hv@cluster0.zxnap1i.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "teste";
+const collectionName = "alunos";
+
+app.get('/alunos', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    const documents = await collection.find({}).toArray();
+    res.json(documents);
+
+    client.close();
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+    res.status(500).json({ error: 'Erro ao conectar ao banco de dados' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
